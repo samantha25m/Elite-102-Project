e1674dd4cf261d2df20b1dce3640e61cde897d92
samantha25m <samantha.b.murphy63@gmail.com>
2023-04-13 18:43:18 -0400

    data functions

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('....')
+import mysql.connector
+
+def select_data(connection):
+    cursor=connection.cursor()
+    testQuery= "SELECT * FROM pet_table"
+    cursor.execute(testQuery)
+    for item in cursor:
+        print(item)
+    cursor.close()
+
+def insert_data(connection):
+    cursor=connection.cursor()
+    addData = "INSERT INTO table_name (column1, column2, column3…) VALUES (value1,value2,value3)"
+    cursor.execute(addData)
+    connection.commit()
+    cursor.close()
+
+connection = mysql.connector.connect(user = 'root', database = 'example_1', password = 'Bippyb00')
+
+select_data(connection)
+
+connection.close()
